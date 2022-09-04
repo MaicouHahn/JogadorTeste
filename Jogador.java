ce397e883319ac0feaa02eb39b95fb2d7070c223
Maicou <maicouhahn11@gmail.com>
2022-09-04 14:38:53 -0300

    Interacao entre j1 e j2

--- Jogador.java
+++ Jogador.java
@@ -11,22 +11,22 @@ public class Jogador {
     public Jogador(int n){
         
         jog=n;
-
-        HP=new SecureRandom().nextFloat(100);
-        For=new SecureRandom().nextFloat(20);
-        Int=new SecureRandom().nextFloat(20);
-        Dex=new SecureRandom().nextFloat(20);
+        //gera um jogador aleatorio com atributos basicos
+        HP=new SecureRandom().nextFloat(1,100);
+        For=new SecureRandom().nextFloat(1,20);
+        Int=new SecureRandom().nextFloat(1,20);
+        Dex=new SecureRandom().nextFloat(1,20);
         
         //System.out.printf("\nJogador %d criado\nHP: %d\nForça: %d\nInt: %d\nDestreza: %d\n",n,(HP+1),For,Int,Dex);
 
         }
     public void info(){
         System.out.println("\nJogador "+jog);
-        System.out.println(this.morto ? "Jogador morto":"");
-        System.out.println("Vida..............: "+HP);
-        System.out.println("Força.............: "+For);
-        System.out.println("Inteligencia......: "+Int);
-        System.out.println("Destreza..........: "+Dex);
+        System.out.printf("%s",this.morto ? "Jogador morto":"");
+        System.out.printf("Vida..............: %.2f\n",HP);
+        System.out.printf("Força.............: %.2f\n",For);
+        System.out.printf("Inteligencia......: %.2f\n",Int);
+        System.out.printf("Destreza..........: %.2f\n",Dex);
 
     }
     public float getHP(){
@@ -41,46 +41,62 @@ public class Jogador {
     public float getDex(){
         return this.Dex;
     }
-    public float causarDano(){
+    public float causarDano(){ //calcula o dano baseado na força inicial do personagem
         
         if(For >=15){
-            this.AumD=1.2f;
+            this.AumD=1.2f;//Aumenta dano em 20%
 
             this.Cdano = ((this.For+this.Dex)/2)*AumD;
         }
         else if(this.For < 15 && this.For >= 10 ){
 
-            this.AumD=1.1f;
+            this.AumD=1.1f;//Aumenta dano em 10%
 
             this.Cdano = ((this.For+this.Dex)/2)*AumD;
         }
         else{
-              this.Cdano = ((this.For+this.Dex)/2)*AumD;  
+              this.Cdano = ((this.For+this.Dex)/2);  
         }
 
         return this.Cdano;
     }
-    public void receberDano(int Rdano)
+    public void receberDano(float Rdano)//Calcula a reduçao de dano baseado na força
     {
         this.Rdano=Rdano;
-        
-        this.HP = this.HP - Rdano;
+        if(this.For >= 15){
+
+            this.RedD = 0.7f;//Reduz o dano em 30%
+
+            this.Rdano=this.Rdano*this.RedD;
+        }
+        else if(this.For >=10 && this.For < 15){
+            this.RedD=0.9f;//reduuz o danoo em 10%
+
+               this.Rdano=this.Rdano*this.RedD;
+        }
+
+        this.HP = this.HP - this.Rdano; //pega o dano testado e diminui no hp total
 
         if(this.HP <= 0){
             
             this.HP=0;
-            this.morto=true;
+            this.morto=true; //se o jogador morrer ele retorna true e zera o hp
         }
-        
-       }
-    public boolean getMorto(){
+    }
+    public boolean getMorto(){//retorna o estado do jogador
         return this.morto;
      }  
     public float setFor(float For){
-        return this.For=For;
+        return this.For = For;
     }
     public float setDex(float Dex){
-        return this.Dex=Dex;
+        return this.Dex = Dex;
+    }
+    public float setInt(float Int){
+        return this.Int = Int;
+    }
+    public float setHP(float HP){
+        return this.HP = HP;
     }
 
     }
--- Jogadorclasse.java
+++ Jogadorclasse.java
@@ -7,50 +7,28 @@ public class Jogadorclasse {
     public static void main(String[]args){
         
         Jogador j1 = new Jogador(1);
-        
+        Jogador j2 = new Jogador(2);
         float d1,d2;
-        
-        j1.setFor(14);
-        j1.setDex(10);
+
+    
+        j1.info();
+        j2.info();
+
         d1=j1.causarDano();
-        System.out.printf("Jogador 1 For: %.2f\nDex: %.2f\nDano: %.2f \n\n",j1.getFor(),j1.getDex(),d1);
+        
+        System.out.printf("O jogador 1 emboscou o jogador 2 causando %.2f de dano\n",d1);
+        
+        j2.receberDano(d1);
 
-        j1.setFor(15);
-        j1.setDex(10);
-        d2=j1.causarDano();
-        System.out.printf("Após incremento\nJogador 1 For: %.2f\nDex: %.2f\nDano: %.2f ",j1.getFor(),j1.getDex(),d2);
+        if(j2.getMorto()==true){
 
-        /* 
-        while( j1.getMorto() != true)
-
+            System.out.println("O Dano matou o jogador 2");
+        }
+        else
         {
-            System.out.println("Causar dano em j1: ");
-           
-            bt=scan.nextInt();
-            
-            if(bt==1){
-
-                d1=new SecureRandom().nextInt(25);
-                j1.dano(d1);
-
-                if(d1>20){
-                    System.out.println("j1 recebeu dano critico de "+d1);
-                    System.out.println("HP: "+j1.getHP());
-                }
-                else
-                {
-                    System.out.println("j1 recebeu dano de: "+d1);
-                    System.out.println("HP: "+j1.getHP());
-                }
-                
-                
-
-            }
-            
-            System.out.println(j1.getMorto() ? "Jogador Morreu" : "");
-            
+            System.out.printf("O jogador 2 recebeu %.2f de dano e agora esta com %.2f de HP\n",d1,j2.getHP());
 
         }
-        */
+
    }
 }
